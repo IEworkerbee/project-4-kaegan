3f5b26bf8a42fae2953100c479e17558a120f3ea
IEusername <kizaegan@gmail.com>
2023-02-22 23:15:17 -0800

    Commits working webapplication interaction between calc.html and flask_brevets.py Also includes test suites for acp_times logic and updated README.md

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -51,14 +51,18 @@ def _calc_times():
     """
     app.logger.debug("Got a JSON request")
     km = request.args.get('km', 999, type=float)
+    brevet_length = request.args.get('brevet_length', 200, type=int)
+    begin_date = request.args.get('begin_date', '2021-01-01T00:00', type=str)
     app.logger.debug("km={}".format(km))
     app.logger.debug("request.args: {}".format(request.args))
     # FIXME!
     # Right now, only the current time is passed as the start time
     # and control distance is fixed to 200
     # You should get these from the webpage!
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
+    
+    open_time = acp_times.open_time(km, brevet_length, arrow.get(begin_date)).format('YYYY-MM-DDTHH:mm')
+    app.logger.debug("DID IT HAPPEN???")
+    close_time = acp_times.close_time(km, brevet_length, arrow.get(begin_date)).format('YYYY-MM-DDTHH:mm')
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
 
--- brevets/run_tests.sh
+++ brevets/run_tests.sh
@@ -3,4 +3,5 @@
 for t in tests/*.py
 do
     nosetests $t
+    sleep 10m
 done
